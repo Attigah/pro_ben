d9c24c44380ae775456085d075e553e56946727a
Attigah <bkeattigah@st.ug.edu.gh>
2024-06-21 00:37:09 +0000

    Button counter application created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
+import {useState} from 'react';
 
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name:{props.LastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
+
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+
   return (
     <div className="App">
-      <Person name={'John'} LastName={'Doe'} age={25}/>
-      <Person name={'Mary'} LastName={'Doa'} age={24}/>
-      <Person />
-      <Person />
-      <Person />
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>  
     </div>
   );
 }
