36dc59d5393496c82c704a064311dfb264665a41
Attigah <bkeattigah@st.ug.edu.gh>
2024-07-10 20:36:17 +0000

    creating jsx for movie app

--- button_tutorial/src/App.js
+++ button_tutorial/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//http://www.omdbapi.com/?i=tt3896198&apikey=2e5d46cf
+const API_URL = 'http://www.omdbapi.com?apikey=2e5d46cf';
+
+
+//API SETTINGS.abk
+const App = () => {
+ 
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
+     return (
+       <div className="app">
+        <h1>MovieLand
+            <div className="search">
+                <input
+                placeholder="Search for moives"
+                value="Superman"
+                onChange={() => {}}
+                />
+                <img
+                src={searchIcon}
+                alt="search"
+                onClick={() => {}}
+                />
+
+            </div>
+        </h1>
+
+       </div>>
+    );
 }
 
 export default App;
